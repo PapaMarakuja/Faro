7f1bf4284c48a0d7b6258805f965690e8beca86a
Rafael Senac <vampirorp13@gmail.com>
2022-08-16 21:25:49 -0300

    base

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 function typeWriter(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 15 * i);
-    });
-  }
+	const textoArray = elemento.innerHTML.split('');
+	elemento.innerHTML = '';
+	textoArray.forEach((letra, i) => {
+		setTimeout(() => (elemento.innerHTML += letra), 15 * i);
+	});
+}
 
-  const titulo = document.querySelector('h1');
+const titulo = document.querySelector('h1');
 
+typeWriter(titulo);
 
-  typeWriter(titulo);
+function colcoarBtns() {}
+
+function runAudio(btn) {
+	switch (btn) {
+		case 00:
+			console.log('btn 01');
+			break;
+		case 01:
+			console.log('btn 02');
+			break;
+	}
+}
